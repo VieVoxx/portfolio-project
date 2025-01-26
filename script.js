dd5d234ec6fc29d5a2c0de76dcaacd47f610f94e
Vie <vievoxx.games@gmail.com>
2025-01-26 15:34:51 -0500

    third commit

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const secOne = document.querySelector('.sec-1');
 const thumbnails = document.querySelectorAll('.thumbnail');
 const thumbCont = document.querySelector('.thumbnails-container');
 const thumbContRectTop = thumbCont.getBoundingClientRect().top
+const cards = document.querySelectorAll('.card');
 
 thumbnails.forEach((thumbnail, index) => {
     thumbnail.style.zIndex = thumbnails.length - index;
+})
+
+cards.forEach((card, index) => {
+    card.style.zIndex = cards.length - index;
 })
 
 window.addEventListener('scroll', () => {
@@ -14,12 +19,14 @@ window.addEventListener('scroll', () => {
     
     index = Math.floor(index);
     for ( let i=0; i < thumbnails.length - 1; i++) {
-        let newY = (thumbContRectTop - thumbnails[i].getBoundingClientRect().top) - 800;
+        let newY = -800;
 
         if (i <= index) {
             thumbnails[i].style.transform = `translateY(${newY}px)`;
+            cards[i].style.transform = `translateY(${newY}px)`;
         } else {
             thumbnails[i].style.transform = `translateY(0%)`;
+            cards[i].style.transform = `translateY(0%)`;
         }
     } 
 })
